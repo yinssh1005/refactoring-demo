d441a97e98e6999aeb49d0459d66b8ede61e095e
Ethan Yin <ethan.yin@jetbrains.com>
2024-11-18 12:13:18 +0800

    Add new class `IntroPara` with a sample method
    This commit introduces a new Java class `IntroPara` containing a method that adds two integers and prints the result, along with an uninitialized string.

---
+++ src/main/java/u/t/t/IntroPara.java
@@ -0,0 +1,10 @@
+package u.t.t;
+
+public class IntroPara {
+    public void method(int a, int b) {
+        int c = a + b;
+        System.out.println(c);
+        String d = "to be initialized";
+        System.out.println(d);
+    }
+}
